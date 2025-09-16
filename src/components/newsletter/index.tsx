c0cf756676f3879fcc81f685fdbb1997ed8763a4
paffexe <kamolov607@gmail.com>
2025-09-16 22:17:03 +0500

    1st commit

---
+++ src/components/newsletter/index.tsx
@@ -0,0 +1,11 @@
+import { memo } from 'react';
+
+const NewsLetter = () => {
+  return (
+    <div className="NewsLetter">
+      <h2>NewsLetter</h2>
+    </div>
+  );
+};
+
+export default memo(NewsLetter);
