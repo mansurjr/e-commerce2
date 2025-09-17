cc9edb9fd9d97ed017c6bd200f7e271324d0c042
Azizbek Programmer <azizbekmirzavaliyev31@gmail.com>
2025-09-17 10:54:47 +0500

    shop done

---
+++ src/pages/shop/components/shopHero.tsx
@@ -0,0 +1,26 @@
+import { memo } from "react";
+import shopHeroImg from "../../../assets/shop/shopHero.png"
+
+const ShopHero = () => {
+  return (
+    <div
+      className="container relative w-full h-[300px] md:h-[400px] flex items-center justify-center bg-cover bg-center py-[50px]"
+      style={{ backgroundImage: `url(${shopHeroImg})` }}
+    >
+      <div className="absolute inset-0 bg-white/20"></div>
+      <div className="relative text-center text-black px-4">
+        <p className="text-sm text-gray-600 mb-2">
+          {" "}
+          Home <span className="mx-1">›</span>{" "}
+          <span className="font-medium">Shop</span>
+        </p>
+        <h1 className="text-3xl md:text-4xl font-bold mb-2">Shop page</h1>
+        <p className="text-gray-700 text-base md:text-lg">
+          Let’s design the place you always imagined.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default memo(ShopHero);
--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -1,118 +1,14 @@
-import {
-  memo,
-  useCallback,
-  useEffect,
-  useState,
-  type ChangeEvent,
-} from "react";
-import { useFetch } from "../../hooks/useFetch";
-import ProductView from "../../components/product-view/ProductView";
-import type { IProduct } from "../../types";
-
-import uchta from "../../assets/shop/3ta.svg";
-import ikkita from "../../assets/shop/2ta.svg";
-import { FaTableCellsLarge } from "react-icons/fa6";
+import { memo } from 'react';
+import Shopcard from './components/shopcard';
+import ShopHero from './components/shopHero';
 
 const Shop = () => {
-  const [order, setOrder] = useState<string>("id-asc");
-  const [skip, setSkip] = useState(0);
-  const [products, setProducts] = useState<IProduct[]>([]);
-  const [category, setCategory] = useState("");
-  const [gridType, setGridType] = useState<"2" | "3" | "4">("4");
-
-  const limit = 4;
-
-  const { data: categories } = useFetch<string[]>("/products/category-list");
-
-  const { data, loading, error } = useFetch<{
-    products: IProduct[];
-    total: number;
-  }>(`/products/${category}`, {
-    limit,
-    skip,
-    sortBy: order.split("-")[0],
-    order: order.split("-")[1],
-  });
-
-  useEffect(() => {
-    if (data?.products) {
-      setProducts((prev) => [...prev, ...data.products]);
-    }
-  }, [data]);
-
-  const setDefaultValue = useCallback(() => {
-    setProducts([]);
-    setSkip(0);
-  }, []);
-
-  const handleChangeOrder = (e: ChangeEvent<HTMLSelectElement>) => {
-    setOrder(e.target.value);
-    setDefaultValue();
-  };
-
-  const handleChangeCategory = (e: ChangeEvent<HTMLSelectElement>) => {
-    const { value } = e.target;
-    setCategory(value ? `category/${value}` : value);
-    setDefaultValue();
-  };
-
   return (
-    <div className="">
-      <h2>Shop</h2>
-
-      <div className="container flex justify-between">
-        <div className="">
-          <select className="border rounded-full" onChange={handleChangeOrder}>
-            <option value="id-asc">OmmaBop</option>
-            <option value="rating-desc">Reyting</option>
-            <option value="price-desc">Qimmat</option>
-            <option value="price-asc">Arzon</option>
-          </select>
-
-          <select
-            onChange={handleChangeCategory}
-            className="border rounded-full"
-          >
-            <option value="">All</option>
-            {categories?.map((item) => (
-              <option key={item} value={item}>
-                {item}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <button onClick={() => setGridType("4")}>
-            <FaTableCellsLarge size={40} />
-          </button>
-          <button onClick={() => setGridType("3")}>
-            <img src={uchta} alt="" />
-          </button>
-          <button onClick={() => setGridType("2")}>
-            <img src={ikkita} alt="" />
-          </button>
-        </div>
+    <div className="Shop">
+      <div className='pb-[50px]'>
+      <ShopHero/>
       </div>
-
-      <ProductView
-        data={products}
-        loading={loading}
-        error={error}
-        gridType={gridType}
-      />
-
-      {loading && <div className="text-center text-5xl">Loading...</div>}
-
-      {data?.total && data.total > skip + limit && (
-        <div className="flex justify-center py-6">
-          <button
-            className="border rounded-full w-28"
-            onClick={() => setSkip((p) => p + limit)}
-          >
-            See more
-          </button>
-        </div>
-      )}
+      <Shopcard/>
     </div>
   );
 };
